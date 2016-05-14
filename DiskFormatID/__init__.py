9b4883bb8af514aef72cd17ec87b3098c668a03f
Kam Woods <kamwoods@gmail.com>
2016-05-14 14:21:54 -0400

    Updated for setuptools

---
+++ DiskFormatID/__init__.py
@@ -0,0 +1,12 @@
+# Placeholder for future packaging
+__license__ = "GPL 3.0"
+__version__ = "0.1.0"
+#__all__ = ['libredact']
+
+#from .diskID_config import *
+
+#try:
+#    from .diskID import diskID_sample
+#except ImportError:
+#    import warnings
+#    warnings.warn("Dep X not installed. Needed for diskID.")
